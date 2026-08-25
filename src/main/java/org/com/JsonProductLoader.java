e3c5a328327fea28fb81fe95ee25c9ab91fcfc15
Alexander <allfreetome@gmail.com>
2026-08-25 15:28:41 +0300

    fixed loadProducts()

--- src/main/java/org/com/JsonProductLoader.java
+++ src/main/java/org/com/JsonProductLoader.java
@@ -1,5 +1,8 @@
 package org.com;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -8,22 +11,17 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 public class JsonProductLoader {
-    public List<Product> loadProducts() throws IOException {
+    public List<Product> loadProducts() {
         Gson gson = new GsonBuilder().create();
-        try (InputStream inputStream = JsonProductLoader.class.getClassLoader().getResourceAsStream("products.json")) {
-            if (inputStream == null) {
-                throw new IllegalArgumentException("File not found: products.json");
-            }
-            try (Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
-                Product[] productArray = gson.fromJson(reader, Product[].class);
-                return Arrays.asList(productArray);
+        try (InputStream inputStream = JsonProductLoader.class.getClassLoader().getResourceAsStream("products.json")){
+            assert inputStream != null;
+            try (Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)){
+                Product[] productsArray = gson.fromJson(reader, Product[].class);
+                return Arrays.asList(productsArray);
             }
         } catch (Exception e) {
-            throw new IOException(e);
+            throw new RuntimeException(e);
         }
     }
 }
