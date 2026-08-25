3b6b1105f07aea508b132a4970fa6a971909da1d
Alexander <allfreetome@gmail.com>
2026-08-25 15:31:58 +0300

    fixed validator

--- src/main/java/org/com/tools/RUPhoneNumberValidator.java
+++ src/main/java/org/com/tools/RUPhoneNumberValidator.java
@@ -1,19 +1,12 @@
 package org.com.tools;
 
-import org.com.repos.UserRepo;
-
 import java.util.regex.Pattern;
 
 public class RUPhoneNumberValidator {
-    static UserRepo userRepo;
     private static final Pattern PATTERN = Pattern
             .compile("^(\\+7|7|8)?[\\s\\-]?\\(?[489][0-9]{2}\\)?[\\s\\-]?[0-9]{3}[\\s\\-]?[0-9]{2}[\\s\\-]?[0-9]{2}$");
 
     public static boolean validateNumber(String phoneNumber) {
-//        if (userRepo.findByPhoneNumber(phoneNumber).isPresent()){
-//            System.out.println("phNum exist");
-//            return false;
-//        }
         return PATTERN.matcher(phoneNumber).matches();
     }
 }
