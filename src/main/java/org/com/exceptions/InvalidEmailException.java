85c92fc80c8fd0952d6637b4579a67c15a394dc4
Alexander <allfreetome@gmail.com>
2026-08-28 16:42:37 +0300

    class InvalidEmailException now have actual error message

--- src/main/java/org/com/exceptions/InvalidEmailException.java
+++ src/main/java/org/com/exceptions/InvalidEmailException.java
@@ -1,7 +1,14 @@
 package org.com.exceptions;
 
-public class InvalidEmailException extends Exception {
-    public InvalidEmailException(String message) {
-        super(message);
+public class InvalidEmailException extends RuntimeException {
+    public InvalidEmailException() {
+        super("""
+                ?EmailIncorrect?
+                    Valid email address:
+                    email@example.com
+                    firstname.lastname@example.com
+                    firstname+lastname@example.com
+                    email@subdomain.example.com
+                    1234567890@example.com""");
     }
 }
