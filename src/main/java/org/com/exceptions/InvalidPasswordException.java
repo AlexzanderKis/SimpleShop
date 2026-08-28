84cef35e417cddb7f3d533ea42b138635f93d8c0
Alexander <allfreetome@gmail.com>
2026-08-28 16:42:51 +0300

    class InvalidPasswordException now have actual error message

--- src/main/java/org/com/exceptions/InvalidPasswordException.java
+++ src/main/java/org/com/exceptions/InvalidPasswordException.java
@@ -1,7 +1,13 @@
 package org.com.exceptions;
 
-public class InvalidPasswordException extends Exception {
-    public InvalidPasswordException(String message){
-        super(message);
+public class InvalidPasswordException extends RuntimeException {
+    public InvalidPasswordException(){
+        super("""
+                "?PasswordIncorrect?"
+                    Ensure there is at least one lowercase letter.
+                    Ensure there is at least one uppercase letter.
+                    Ensure there is at least one number digit.
+                    Ensure there is at least one special character from set [@#$%^&+=].
+                    Requires the total length of the password to be 8 or more characters.""");
     }
 }
