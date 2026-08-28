5c499e41b5f2d4f9ca4ef0f10a7158ab0b6d1f27
Alexander <allfreetome@gmail.com>
2026-08-28 16:42:58 +0300

    class InvalidPhoneNumberException now have actual error message

--- src/main/java/org/com/exceptions/InvalidPhoneNumberException.java
+++ src/main/java/org/com/exceptions/InvalidPhoneNumberException.java
@@ -1,7 +1,12 @@
 package org.com.exceptions;
 
-public class InvalidPhoneNumberException extends Exception {
-    public InvalidPhoneNumberException(String message){
-        super(message);
+public class InvalidPhoneNumberException extends RuntimeException {
+    public InvalidPhoneNumberException() {
+        super("""
+                ?PhoneNumberIncorrect?
+                    Valid phone number format:
+                    +7 (911) 123-45-67
+                    84951234567
+                    7-495-123-45-67""");
     }
 }
