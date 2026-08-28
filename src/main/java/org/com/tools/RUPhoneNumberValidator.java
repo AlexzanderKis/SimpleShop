5cac877905e8513d8809c643f4f00f911c510669
Alexander <allfreetome@gmail.com>
2026-08-28 16:45:10 +0300

    validateNumber() now throw correct InvalidPhoneNumberException

--- src/main/java/org/com/tools/RUPhoneNumberValidator.java
+++ src/main/java/org/com/tools/RUPhoneNumberValidator.java
@@ -1,5 +1,7 @@
 package org.com.tools;
 
+import org.com.exceptions.InvalidPhoneNumberException;
+
 import java.util.regex.Pattern;
 
 public class RUPhoneNumberValidator {
@@ -7,6 +9,9 @@ public class RUPhoneNumberValidator {
             .compile("^(\\+7|7|8)?[\\s\\-]?\\(?[489][0-9]{2}\\)?[\\s\\-]?[0-9]{3}[\\s\\-]?[0-9]{2}[\\s\\-]?[0-9]{2}$");
 
     public static boolean validateNumber(String phoneNumber) {
-        return PATTERN.matcher(phoneNumber).matches();
+        if (PATTERN.matcher(phoneNumber).matches()){
+            return true;
+        } throw new InvalidPhoneNumberException();
+//        return PATTERN.matcher(phoneNumber).matches();
     }
 }
